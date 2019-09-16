e2682951da2863665a2438eb03fa3ada42fb8471
Matthew Anderson <44532345+qztv5@users.noreply.github.com>
2019-09-16 14:25:15 -0400

    Update db.js

--- lms/dao/db.js
+++ lms/dao/db.js
@@ -4,8 +4,7 @@ var connection = mysql.createConnection({
     host     : 'library.cbaxpbusjdjz.us-east-1.rds.amazonaws.com',
     user     : process.env.DBUN,
     password : process.env.DBPASS,
-    database : 'library',
-    port     : 3305
+    database : 'library'
 });
 
 module.exports = connection;
